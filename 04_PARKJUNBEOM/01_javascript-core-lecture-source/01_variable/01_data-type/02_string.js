47ca6d5524baa47d5e012da09021c190f5cb63b9
junbem <junbem0728@naver.com>
2025-04-29 14:03:13 +0900

    커밋

---
+++ 04_PARKJUNBEOM/01_javascript-core-lecture-source/01_variable/01_data-type/02_string.js
@@ -0,0 +1,15 @@
+var string; // 변수 선언
+string = 'javascript';
+string = "javascript"
+string = `javascript`   // 백틱(`) -> 줄바꿈 가능
+
+console.log('string: ', string );
+
+var str = `안녕하세요
+    반갑습니다.`
+console.log('str: ', str);
+
+var lastName = '홍';
+var firstName = '길동'
+console.log(`제 이름은 ${lastName}${firstName}입니다.`);
+
---
+++ 04_PARKJUNBEOM/01_javascript-core-lecture-source/01_variable/02_dynamically-typed-language/01_dynamically-typed-language.js
@@ -0,0 +1,30 @@
+//JavaScript는 동적 타입 언어이다.
+// => 데이터 타입을 사전에 선언하지 않는다.
+// => 변수 선언이 아니라 할당에 의해 타입이 결정되며
+// 재할당에 의해 동적으로 변수의 타입이 변화할 수 있다.
+var test;
+console.log(typeof test);
+
+test = 1;
+console.log(typeof test);
+
+test = true;
+console.log(typeof test);
+
+test = null;
+console.log(typeof test);
+// 자바스크립트의 초기 버그이지만
+// 영향을 줄수 있어서 아직까지 수정되지 못하고 있음
+
+test = Symbol;
+console.log(typeof test);
+
+test = {};              // 객체
+console.log(typeof test);
+
+test = [];              // 배열
+console.log(typeof test);
+
+test = function(){};    // 함수
+console.log(typeof test);
+
